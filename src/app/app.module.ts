4d898d7960cabe7e6724862c079c7469621644b8
Kinetsu <admin@aimzone.net>
2019-09-15 11:08:36 -0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { TranslateService } from './translate.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +12,7 @@ import { NavBarTopComponent } from './nav-bar-top/nav-bar-top.component';
 import { CarouselMainComponent } from './home/carousel-main/carousel-main.component';
 import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './home/news/news.component';
+import { TranslatePipe } from './translate.pipe';
 
 @NgModule({
   declarations: [
@@ -16,15 +20,17 @@ import { NewsComponent } from './home/news/news.component';
     NavBarTopComponent,
     CarouselMainComponent,
     HomeComponent,
-    NewsComponent
+    NewsComponent,
+    TranslatePipe
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
